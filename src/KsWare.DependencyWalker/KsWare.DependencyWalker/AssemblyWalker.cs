ddeadfe6bc0a4d901939b207892be93efd17ff6b
SchreinerK <ks@ksware.de>
2018-03-12 19:23:42 +0100

    add AssemblyWalker, add SignatureHelper

--- src/KsWare.DependencyWalker/KsWare.DependencyWalker/AssemblyWalker.cs
+++ src/KsWare.DependencyWalker/KsWare.DependencyWalker/AssemblyWalker.cs
@@ -33,15 +33,15 @@ namespace KsWare.DependencyWalker {
 			return null;
 		}
 
-		public static AssemblyInfo LoadAssembly(string fileName) {
+		public static MyAssemblyInfo LoadAssembly(string fileName) {
 			var assembly = Assembly.ReflectionOnlyLoadFrom(fileName);
-			return new AssemblyInfo(assembly) {
+			return new MyAssemblyInfo(assembly) {
 				AssemblyName = assembly.GetName(false),
 				FileName = fileName
 			};
 		}
 
-		public static AssemblyInfo LoadAssembly(AssemblyName name, string folder) {
+		public static MyAssemblyInfo LoadAssembly(AssemblyName name, string folder) {
 			Assembly assembly;
 			try {
 				assembly = Assembly.ReflectionOnlyLoad(name.FullName);
@@ -58,14 +58,14 @@ namespace KsWare.DependencyWalker {
 					EnableAssemblyResolver = false;
 				}
 			}
-			return new AssemblyInfo(assembly) {
+			return new MyAssemblyInfo(assembly) {
 				AssemblyName = name,
 				FileName = assembly.Location
 			};
 		}
 
-		public static void LoadAssembly(AssemblyInfo assemblyInfo, string folder) {
-			AssemblyInfo ai;
+		public static void LoadAssembly(MyAssemblyInfo assemblyInfo, string folder) {
+			MyAssemblyInfo ai;
 			if (assemblyInfo.AssemblyName != null) {
 				ai = LoadAssembly(assemblyInfo.AssemblyName, folder);
 			}
@@ -81,8 +81,8 @@ namespace KsWare.DependencyWalker {
 			assemblyInfo.FileName = ai.FileName;
 		}
 
-		public static void LoadDependencies(AssemblyInfo assemblyInfo, bool recursive) {
-			assemblyInfo.ReferencedAssemblies = assemblyInfo.Assembly.GetReferencedAssemblies().Select(n => new AssemblyInfo(n)).ToArray();
+		public static void LoadDependencies(MyAssemblyInfo assemblyInfo, bool recursive) {
+			assemblyInfo.ReferencedAssemblies = assemblyInfo.Assembly.GetReferencedAssemblies().Select(n => new MyAssemblyInfo(n)).ToArray();
 			foreach (var rai in assemblyInfo.ReferencedAssemblies) {
 				LoadAssembly(rai, Path.GetDirectoryName(assemblyInfo.FileName));
 			}
@@ -91,24 +91,80 @@ namespace KsWare.DependencyWalker {
 				LoadDependencies(rai, true);
 			}
 		}
+
+		public static MyTypeInfo[] GetExportedTypes(Assembly assembly, bool includeMembers) {
+			var types = assembly.GetExportedTypes().Select(t=> new MyTypeInfo(t)).ToArray();
+
+			if (includeMembers) {
+				foreach (var typeInfo in types) {
+					typeInfo.DisplayName = SignatureHelper.ForCompare.Sig(typeInfo.Type);
+				}
+
+				foreach (var typeInfo in types) {
+					var sm = typeInfo.Type.GetMembers(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly)
+						.Select(m => new MyMemberInfo(typeInfo, m));
+					var im = typeInfo.Type.GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+						.Select(m => new MyMemberInfo(typeInfo, m));
+					typeInfo.Members = sm.Concat(im).ToArray();
+
+					foreach (var memberInfo in typeInfo.Members) {
+						memberInfo.DisplayName = SignatureHelper.ForCompare.Sig(memberInfo.MemberInfo);
+					}
+				}
+			}
+			return types;
+		}
 	}
 
-	public class AssemblyInfo {
+	public class MyTypeInfo {
 
-		public AssemblyInfo() { }
+		public MyTypeInfo(Type type) { Type = type; }
 
-		public AssemblyInfo(Assembly assembly) {
+		public Type Type { get; }
+
+		public string FullName => Type.FullName;
+
+		public MyMemberInfo[] Members { get; set; }
+
+		public string DisplayName { get; set; }
+
+	}
+
+	public class MyMemberInfo {
+
+		public MyMemberInfo(MyTypeInfo typeInfo, MemberInfo memberInfo) {
+			TypeInfo = typeInfo;
+			MemberInfo = memberInfo;
+		}
+
+		public MyTypeInfo TypeInfo { get; }
+		public MemberInfo MemberInfo { get; }
+		public string DisplayName { get; set; }
+	}
+
+	public class MyAssemblyInfo {
+
+		public MyAssemblyInfo() { }
+
+		public MyAssemblyInfo(Assembly assembly) {
 			Assembly=assembly;
 		}
 
-		public AssemblyInfo(AssemblyName assemblyName) {
+		public MyAssemblyInfo(AssemblyName assemblyName) {
 			AssemblyName=assemblyName;
 		}
 
 		public string FileName { get; set; }
+
 		public AssemblyName AssemblyName { get; set; }
+
 		public Assembly Assembly { get; set; }
-		public AssemblyInfo[] ReferencedAssemblies { get; set; }
+
+		public MyAssemblyInfo[] ReferencedAssemblies { get; set; }
+
+		public MyTypeInfo[] Types { get; set; }
 	}
+
+	
 
 }
--- src/KsWare.DependencyWalker/KsWare.DependencyWalker/MainWindow.xaml.cs
+++ src/KsWare.DependencyWalker/KsWare.DependencyWalker/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KsWare.DependencyWalker {
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -28,8 +29,17 @@ namespace KsWare.DependencyWalker {
 		private void Target() {
 			var ai = AssemblyWalker.LoadAssembly(
 				@"D:\Develop\Extern\GitHub.KsWare\KsWare.Presentation\master\src\KsWare.Presentation.UITestApp\bin\Debug\KsWare.Presentation.UITestApp.exe");
-			AssemblyWalker.LoadDependencies(ai, true);
-			TreeView.ItemsSource = new[] {ai};
+//			AssemblyWalker.LoadDependencies(ai, true);
+//			TreeView.ItemsSource = new[] {ai};
+
+//			Die Abhängigkeit von
+//				Assembly "XYZ" kann nicht aufgelöst werden, da sie nicht vorher geladen wurde. Beim 
+//				Verwenden der ReflectionOnly - APIs müssen  bhängige Assemblys vorher geladen oder über
+//				das ReflectionOnlyAssemblyResolve - Ereignis bei Bedarf geladen werden.'
+
+			AssemblyWalker.LoadDependencies(ai,true);
+			ai.Types = AssemblyWalker.GetExportedTypes(ai.Assembly, true);
+			TypesTreeView.ItemsSource = new[] {ai};
 		}
 	}
 }
